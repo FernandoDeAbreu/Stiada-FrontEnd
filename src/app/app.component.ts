fbcdbe1ff1e5699196511908d78463e1155763c5
fernandodataintegra <fas.abreu@gmail.com>
2025-05-31 09:53:35 -0300

    refactor: update app layout and add routing for members and receita features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from "./core/components/sidenav/sidenav.component";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, MatSidenavModule, MatButtonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,30 @@ export const routes: Routes = [
   {
     path: 'membros/novo',
     loadComponent: () => import('./features/membros/pages/membros-create/membros-create.component').then(m => m.MembrosCreateComponent)
+  },
+  {
+    path: 'membros/editar/:id',
+    loadComponent: () => import('./features/membros/pages/membros-edit/membros-edit.component').then(m => m.MembrosEditComponent)
+  },
+  {
+    path: 'campo',
+    loadComponent: () => import('./features/membros/pages/membros-list/membros-list.component').then(m => m.MembrosListComponent)
+  },
+  {
+    path: 'membros/novo',
+    loadComponent: () => import('./features/membros/pages/membros-create/membros-create.component').then(m => m.MembrosCreateComponent)
+  },
+  {
+    path: 'membros/editar/:id',
+    loadComponent: () => import('./features/membros/pages/membros-edit/membros-edit.component').then(m => m.MembrosEditComponent)
+  },
+  {
+    path: 'membros/details/:id',
+    loadComponent: () => import('./features/membros/pages/membros-details/membros-details.component').then(m => m.MembrosDetailsComponent)
+  },
+  {
+    path: 'receita/lancar/:membro-id',
+    loadComponent: () => import('./features/receita/pages/lancar-receita/lancar-receita.component').then(m => m.LancarReceitaComponent)
   }
 
 ];
